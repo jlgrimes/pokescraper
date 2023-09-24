927189377ac65fc01ff01febd87193cdda28fbf7
Jared Grimes <jaredlgrimes@hotmail.com>
2023-09-24 11:31:20 -0400

    correct parsing and function execution

--- PokescraperTrigger/__init__.py
+++ PokescraperTrigger/__init__.py
@@ -1,24 +1,34 @@
 import logging
 
+from standings import mainWorker
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    id = req.params.get('id')
+    if not id:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            id = req_body.get('id')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    url = req.params.get('url')
+    if not url:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            url = req_body.get('url')
+
+    if id and url:
+        ret = mainWorker(id, url, False, False)
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+             "Missing required params id and url",
+             status_code=400
         )
